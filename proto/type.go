d8d54891765811b837f917e141e41025688b6fc2
Paulo Borges <pborges475@gmail.com>
2019-12-15 10:13:27 -0800

    Add fan device type

--- proto/type.go
+++ proto/type.go
@@ -7,6 +7,7 @@ type DeviceType string
 const (
 	DeviceTypeCamera     DeviceType = "action.devices.types.CAMERA"
 	DeviceTypeLight      DeviceType = "action.devices.types.LIGHT"
+	DeviceTypeFan      DeviceType = "action.devices.types.FAN"
 	DeviceTypeOutlet     DeviceType = "action.devices.types.OUTLET"
 	DeviceTypeSwitch     DeviceType = "action.devices.types.SWITCH"
 	DeviceTypeThermostat DeviceType = "action.devices.types.THERMOSTAT"
