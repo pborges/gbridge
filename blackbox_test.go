6a64bcce4ffdfaba42e5d5e3e97ccbc15dfe36af
Paulo Borges <pborges475@gmail.com>
2019-12-25 11:02:12 -0800

    Issue #26 - Execute in parallel

--- blackbox_test.go
+++ blackbox_test.go
@@ -91,8 +91,10 @@ func TestMultipleExecution(t *testing.T) {
 	buf := bytes.NewBufferString("")
 	json.NewEncoder(buf).Encode(res)
 
-	if strings.TrimSpace(buf.String()) != `{"requestId":"ff36a3cc-ec34-11e6-b1a0-64510650abcf","payload":{"commands":[{"ids":["123","456"],"states":{"on":true,"online":true},"status":"SUCCESS"},{"ids":["123","456"],"states":{"online":true},"status":"ERROR","errorCode":"turnedOff"}]}}` {
-		t.Error("unexpected response")
+	// due to the nature of concurrency we may get the command responses in different orders
+	if strings.TrimSpace(buf.String()) != `{"requestId":"ff36a3cc-ec34-11e6-b1a0-64510650abcf","payload":{"commands":[{"ids":["456"],"states":{"online":true},"status":"ERROR","errorCode":"turnedOff"},{"ids":["123"],"states":{"on":true,"online":true},"status":"SUCCESS"}]}}` &&
+		strings.TrimSpace(buf.String()) != `{"requestId":"ff36a3cc-ec34-11e6-b1a0-64510650abcf","payload":{"commands":[{"ids":["123"],"states":{"on":true,"online":true},"status":"SUCCESS"},{"ids":["456"],"states":{"online":true},"status":"ERROR","errorCode":"turnedOff"}]}}` {
+		t.Error("unexpected response got", strings.TrimSpace(buf.String()))
 	}
 }
 
@@ -131,7 +133,9 @@ func TestSmartHome_encodeDeviceForSyncResponse(t *testing.T) {
 }
 
 func TestStatesResponse(t *testing.T) {
-	home := SmartHome{}
+	home := SmartHome{
+		//Log: log.New(os.Stdout, "", log.LstdFlags),
+	}
 	home.RegisterDevice("test", &BasicLight{
 		BasicDevice: BasicDevice{
 			Id: "123",
